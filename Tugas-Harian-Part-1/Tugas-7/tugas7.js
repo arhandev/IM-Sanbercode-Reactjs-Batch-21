9e45d85f85e0b118f5283551fc1874ee24e31b41
supiraru <farhandewanta11@gmail.com>
2021-01-20 00:17:01 +0700

    Tugas 7

--- Tugas-Harian-Part-1/Tugas-7/tugas7.js
+++ Tugas-Harian-Part-1/Tugas-7/tugas7.js
@@ -107,41 +107,6 @@ class Clock{
     
 }
 
-
-// function Clock({ template }) {
-  
-//     var timer;
-  
-//     function render() {
-//       var date = new Date();
-  
-//       var hours = date.getHours();
-//       if (hours < 10) hours = '0' + hours;
-  
-//       var mins = date.getMinutes();
-//       if (mins < 10) mins = '0' + mins;
-  
-//       var secs = date.getSeconds();
-//       if (secs < 10) secs = '0' + secs;
-  
-//       var output = template
-//         .replace('h', hours)
-//         .replace('m', mins)
-//         .replace('s', secs);
-//         console.log(template)
-//       console.log(output);
-//     }
-  
-//     this.stop = function() {
-//       clearInterval(timer);
-//     };
-  
-//     this.start = function() {
-//       render();
-//       timer = setInterval(render, 1000);
-//     };
-  
-//   }
 var clock = new Clock({template: 'h:m:s'});
 console.log("=======Jawaban Soal 2========")
 clock.start(); 
